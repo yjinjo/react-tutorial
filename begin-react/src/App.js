bcdece14f3bef8be7028d0d382be65dda419d1b0
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-11 16:03:09 +0900

    #1. JSX

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Hello from "./Hello";
+import './App.css';
 
 function App() {
+  const name = 'react'
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
+
   return (
-    <div>
+    <>
       <Hello />
-    </div>
-  );
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+  )
 }
 
 export default App;
