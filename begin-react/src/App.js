dd2adc4ba3b5fc7789b137a80d02ff0462f45b17
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-12 19:17:26 +0900

    #1. useCallback을 사용하여 함수 재사용

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
@@ -14,13 +14,13 @@ function App() {
   })
 
   const { username, email } = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     })
-  }
+  }, [inputs]);
 
   const [users, setUsers] = useState([
     {
@@ -45,7 +45,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -58,13 +58,13 @@ function App() {
       email: '',
     });
     nextId.current += 1;
-  }
+  }, [username, email, users]);
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id));
-  }
+  }, [users]);
 
-  const onToggle = id => {
+  const onToggle = useCallback(id => {
     setUsers(
       users.map(user =>
         user.id === id
@@ -72,7 +72,7 @@ function App() {
           : user
       )
     )
-  }
+  }, [users]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
