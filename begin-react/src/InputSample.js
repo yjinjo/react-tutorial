4bbfbd317636edf726e80be9de60ccdae6f3f7f8
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-12 14:08:37 +0900

    #1. 여러개의 input 상태 관리

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,24 +1,46 @@
 import React, {useState} from "react";
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
-  }
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs, // React 에서 객체를 업데이트를 할 때는 우선 inputs 객체를 복사한 뒤,
+      [name]: value
+    });
+  };
 
   const onReset = () => {
-    setText('');
-  }
+    setInputs({
+      name: '',
+      nickname: '',
+    });
+  };
+
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}/>
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}/>
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} {nickname}
       </div>
     </div>
-  )
+  );
 }
 
 export default InputSample;
