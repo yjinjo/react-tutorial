fe5c2aeb2f3cd995375d0d853a140ad3ad0985cc
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-12 13:19:09 +0900

    #1. 조건부 렌더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -6,7 +6,7 @@ import Wrapper from "./Wrapper";
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial={true}/>
       <Hello color="pink" />
     </Wrapper>
   )
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Hello({color, name}) {
-  return <div style={{color: color}}>안녕하세요. {name}</div>;
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{
+    color: color
+  }}>
+    <b>{isSpecial ? '스페셜 하다': 'not special'}</b>
+    안녕하세요. {name}
+  </div>
+  );
 }
 
 Hello.defaultProps = {
