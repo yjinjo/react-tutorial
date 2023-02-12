d419cdb4aaca32e2e930a34308f513a1c67d20f4
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-12 18:05:38 +0900

    #1. 배열에 항목 수정

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -22,17 +22,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false,
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false,
     }
   ]);
 
@@ -57,6 +60,16 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id
+          ? {...user, active: !user.active }
+          : user
+      )
+    )
+  }
+
 
   return (
     <>
@@ -66,7 +79,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   )
 }
