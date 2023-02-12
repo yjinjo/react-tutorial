c4b216f7b2a6691c8a44ff9c1e17981d0d9db4f2
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-12 18:57:29 +0900

    #1. useMemo를 사용하여 연산한 값 재사용

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -3,16 +3,6 @@ import React, { useEffect } from 'react';
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
 
-  useEffect(() => {
-    console.log('user 값이 설정됨');
-    console.log(user);
-
-    return () => {
-      console.log('user 가 바뀌기 전..');
-      console.log(user);
-    };
-  }, [user]);
-
   return (
     <div>
       <b style={{
